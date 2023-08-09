6d494bae1f14a8cf6c0a579cbcfed47a2504a0db
RayyanDevZone <rayyansyed369@gmail.com>
2023-08-09 21:55:12 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <div className='background'>
+      <nav className='navbar'> <h1 className='navhead'>Travelone</h1>
+      <button className='btn'>Book now ➡</button>
+      </nav>
 
       </div>
       <div className='back2'>
